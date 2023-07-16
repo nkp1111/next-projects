d895dbf60b24dfbbb940be46186d3caad1278372
nkp1111 <neerajp4321@gmail.com>
2023-07-16 16:29:16 +0530

    chore: add readme and live server

--- massively-landing-page/components/Footer.jsx
+++ massively-landing-page/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='py-5'>
-      <div className="text-center text-uppercase text-secondary">
+      <div className="text-center text-uppercase text-white">
         @ untitled
         <span className="mx-3 d-inline-block">|</span> design: html5 up
         <span className="mx-3 d-inline-block">|</span>
