3633e92fa54d80b4a92f90a669dc27b91a5a63a0
nkp1111 <neerajp4321@gmail.com>
2023-08-13 16:14:53 +0530

    feat: add landing page

--- student-time-table-planner/app/page.tsx
+++ student-time-table-planner/app/page.tsx
@@ -1,94 +1,33 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import Link from "next/link";
+import frontImage from "@/public/images/stock-image.jpg"
+import styles from "@/app/utils.module.css"
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <main className={`${styles.height_full}`}>
+      <div className="container-fluid h-100">
+        <div className="row h-100 d-flex align-items-center">
+          <div className="col-md-6 col-12 order-2">
+            <h1>Time Table Planner</h1>
+            <p className="mt-3 pe-5">
+              A single place to plan / draft / manage your classes. Create your timetable and have a look at the calendar and see all your scheduled classes in one place. You can also unenroll in the courses you don&apos;t like.
+            </p>
+            <div className="btn-holder mt-4">
+              <Link role="button" className="btn btn-md btn-danger px-4" href="/register">Register</Link>
+              <Link role="button" className="btn btn-md bg-white ms-4 shadow-lg px-4" href="/login">Login</Link>
+            </div>
+          </div>
+
+          <div className="col-md-6 col-12">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+              src={frontImage}
+              alt="main sitting in front of computer checking screen for data"
+              width={640}
+              height={354}
             />
-          </a>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
