e5da700ae33d7d9c6803def31e93289be23e4907
nkp1111 <neerajp4321@gmail.com>
2023-08-24 16:33:08 +0530

    fix: env variables for nextjs client code

--- student-time-table-planner/constant/index.ts
+++ student-time-table-planner/constant/index.ts
@@ -1,3 +1,3 @@
-export const SERVER_URL: string = process.env.SERVER_URL || "http://localhost:3000";
+export const SERVER_URL: string = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 
-export const SECRET_JWT_TOKEN: string = process.env.SECRET_JWT_TOKEN || "secret";
+export const SECRET_JWT_TOKEN: string = process.env.NEXT_PUBLIC_SECRET_JWT_TOKEN || "secret";
