bcecfb9ebffb1b481b60045db81398cb3414091b
nkp1111 <neerajp4321@gmail.com>
2023-08-25 10:14:02 +0530

    merge: undo mongoose logs from middleware

--- student-time-table-planner/middleware.ts
+++ student-time-table-planner/middleware.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { SERVER_URL } from "@/constant"
-import mongoose from 'mongoose';
 
 let mongooseConnected = false;
 
@@ -12,12 +11,6 @@ export default async function middleware(request: NextRequest) {
     if (data.success) {
       mongooseConnected = true;
       console.log(data.success);
-      // Check the current connection state
-      if (mongoose.connection.readyState === 1) {
-        console.log('Mongoose is connected to the database.');
-      } else {
-        console.log('Mongoose is not currently connected to the database.');
-      }
     } else {
       console.log(data.error)
     }
