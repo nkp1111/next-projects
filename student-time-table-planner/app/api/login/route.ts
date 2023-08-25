1a007973179ea87926a91ae66ecb88cb99fc8439
nkp1111 <neerajp4321@gmail.com>
2023-08-25 13:42:38 +0530

    fix: change mongo connection method

--- student-time-table-planner/app/api/login/route.ts
+++ student-time-table-planner/app/api/login/route.ts
@@ -2,39 +2,46 @@ import { NextRequest, NextResponse } from "next/server";
 import Student from "@/models/students";
 import { StudentLoginType } from "@/types"
 import { sendAuthToken } from "@/lib/auth/authToken";
-import mongoose from "mongoose";
+import { connectMongo, disconnectMongo } from "@/lib/general/mongoDB";
 
 export async function POST(request: NextRequest) {
   try {
     const { email, password }: StudentLoginType = await request.json();
-    console.log("login api", email, password)
+
+    // if email and password are provided
     if (email && password) {
+      // establish mongo connection
+      const { success, error, client } = await connectMongo();
+      if (error || !client) {
+        return NextResponse.json({ error }, { status: 500 })
+      }
+
       let student;
       try {
-        if (mongoose.connection.readyState === 1) {
-          console.log('Mongoose is connected to the database.');
-        } else {
-          console.log('Mongoose is not currently connected to the database.');
-        }
         student = await Student.findOne({ email })
+        console.log("student data fetched");
       } catch (error) {
         console.log(error, "error on login api")
       }
 
       let passwordMatch = false;
       if (student) {
-        console.log("student found", student, password)
         passwordMatch = await student.comparePassword(password);
       }
 
-      console.log("student and password match", passwordMatch)
       if (!student || !passwordMatch) {
         return NextResponse.json({ error: "Email or password or both is/are incorrect" },
           { status: 400 })
       }
 
+      // disconnect mongo db connection
+      const { success: disconnectSuccess, error: disconnectError } = await disconnectMongo(client);
+      if (disconnectError) {
+        console.log("Error while disconnecting mongo `Login api route`")
+      }
+
       const successMessage = "Student logged In successfully"
-      return sendAuthToken(student, successMessage);
+      return await sendAuthToken(student, successMessage);
     } else {
       return NextResponse.json({ error: "Both email and password are required" }, { status: 400 })
     }
