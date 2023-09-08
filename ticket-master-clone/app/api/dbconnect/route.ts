3219d419f9763af35e7baa70e23107dbf4a1313a
nkp1111 <neerajp4321@gmail.com>
2023-09-08 12:21:10 +0530

    fix: mongo connection auto gen by vercel

--- ticket-master-clone/app/api/dbconnect/route.ts
+++ ticket-master-clone/app/api/dbconnect/route.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import { NextRequest, NextResponse } from 'next/server';
 
-const mongoUrl: string = process.env.MONGO_URL || "http://localhost:8080/ticket_master_clone"
+const mongoUrl: string = process.env.MONGODB_URI || process.env.MONGO_URL || "http://localhost:8080/ticket_master_clone"
 export async function GET(request: NextRequest) {
   try {
     console.log("Trying to connect Mongo DB")
