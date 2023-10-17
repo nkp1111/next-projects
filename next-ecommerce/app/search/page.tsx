a32c54d7af0161f23e91467125ab5450e3648d2b
nkp1111 <neerajp4321@gmail.com>
2023-10-17 23:51:06 +0530

    ✨ feat(ecommerce): add search page

--- next-ecommerce/app/search/page.tsx
+++ next-ecommerce/app/search/page.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import ProductCard from "@/components/productCard"
+import prisma from "@/lib/db/prisma"
 
-export default function Search() {
+
+interface SearchPageParams {
+  searchParams: { query: string }
+}
+
+export async function generateMetadata({ searchParams: { query } }: SearchPageParams) {
+  return {
+    title: `Search ${query}- flow_mazon`
+  }
+}
+
+
+export default async function SearchPage({ searchParams: { query } }: SearchPageParams) {
+  // TODO: add product search for pricing info
+  // let queryPrice: number = 0;
+  // if (["<", ">"].includes(query[0])) {
+  //   try {
+  //     queryPrice = Number(query.slice(1,))
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+
+  const products = await prisma.product.findMany({
+    where: {
+      OR: [
+        { title: { contains: query, mode: "insensitive" } },
+        { description: { contains: query, mode: "insensitive" } },
+      ],
+    },
+    orderBy: { id: "desc" },
+  })
+
+  if (!products || products.length === 0) {
+    return <div className="text-center">No products found.</div>
+  }
+
   return (
-    <div>
-      search page
+    <div className="my-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+      {products.map(product => (
+        <ProductCard key={product.id} product={product} />
+      ))}
     </div>
   )
 }
