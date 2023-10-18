027b30677ee872cc24f158fc97bf5a5a8dd0c7f3
nkp1111 <neerajp4321@gmail.com>
2023-10-18 23:42:22 +0530

    chore(tic-tac-toe):➕🍱 add assets, daisyui package

--- tic-tac-toe-multiplayer/tailwind.config.ts
+++ tic-tac-toe-multiplayer/tailwind.config.ts
@@ -6,15 +6,26 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [{
+      theme1: {
+        "primary": "#a3e635",
+        "secondary": "#ead05b",
+        "accent": "#ce7433",
+        "neutral": "#151e23",
+        "base-100": "#373a3f",
+        "info": "#40b6ce",
+        "success": "#2ee5a2",
+        "warning": "#f3cd62",
+        "error": "#ee6f6d",
+        body: {
+          "background-color": "#333135",
+          "color": "#eef1f7",
+        }
       },
-    },
+    }],
   },
-  plugins: [],
 }
+
 export default config
