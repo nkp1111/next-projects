2e5f5b6c50e2d7baae85b6ea31010b6b2538ac1f
nkp1111 <neerajp4321@gmail.com>
2023-12-24 16:59:54 +0530

    feat: ✨💄add dropdown content for navbar

--- mydukaan-landing-page/components/landing-page/header/index.tsx
+++ mydukaan-landing-page/components/landing-page/header/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import TimeDeal from './time-deal'
+import Navbar from './navbar'
 
 export default function Header() {
 
   return (
     <header className='fixed top-0 left-0 w-full'>
       <TimeDeal />
+      <Navbar />
     </header>
   )
 }
