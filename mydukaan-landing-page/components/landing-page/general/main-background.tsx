18018a2bcbc8686ad590811a61fd1158470955cb
nkp1111 <neerajp4321@gmail.com>
2023-12-27 20:42:45 +0530

    refactor:💄change front page background color

--- mydukaan-landing-page/components/landing-page/general/main-background.tsx
+++ mydukaan-landing-page/components/landing-page/general/main-background.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function MainBackground() {
   return (
-    <div className='absolute top-0 left-0 w-full h-[100vh] bg-slate-400 -z-0'>
+    <div className='absolute top-0 left-0 w-full h-[100vh] -z-0 bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400'>
 
     </div>
   )
