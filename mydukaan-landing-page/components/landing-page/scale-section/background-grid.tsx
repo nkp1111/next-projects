7206d1c56452bc91104de2c727f3ce89279f84ff
nkp1111 <neerajp4321@gmail.com>
2023-12-30 19:16:33 +0530

    feat:💄 add scale section

---
+++ mydukaan-landing-page/components/landing-page/scale-section/background-grid.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function BackgroundGrid() {
+  return (
+    <div className="absolute grid grid-cols-7 left-[10%] bottom-0 opacity-30">
+      <div className="w-32 h-32 border border-gray-300 opacity-5"></div>
+      <div className="w-32 h-32 border border-gray-300 opacity-5"></div>
+      <div className="w-32 h-32 border border-gray-300 opacity-5"></div>
+      <div className="w-32 h-32 border border-gray-300 opacity-5"></div>
+      <div className="w-32 h-32 border border-gray-300 opacity-5"></div>
+      <div className="w-32 h-32 border border-gray-300"></div>
+      <div className="w-32 h-32 border border-gray-300"></div>
+      <div className="w-32 h-32 border border-gray-300"></div>
+      <div className="w-32 h-32 border border-gray-300"></div>
+      <div className="w-32 h-32 border border-gray-300"></div>
+      <div className="w-32 h-32 border border-gray-300"></div>
+      <div className="w-32 h-32 border border-gray-300"></div>
+      <div className="w-32 h-32 border border-gray-300"></div>
+      <div className="w-32 h-32 border border-gray-300"></div>
+      <div className="w-32 h-32 border border-gray-300"></div>
+      <div className="w-32 h-32 border border-gray-300"></div>
+      <div className="w-32 h-32 border border-gray-300"></div>
+      <div className="w-32 h-32 border border-gray-300"></div>
+      <div className="w-32 h-32 border border-gray-300"></div>
+      <div className="w-32 h-32 border border-gray-300"></div>
+      <div className="w-32 h-32 border border-gray-300"></div>
+    </div>
+  )
+}
